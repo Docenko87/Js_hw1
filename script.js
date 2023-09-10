0f77fc589370e31aaf7af7bfeb62ccdeb970791f
Alexey Dotsenko <136622178+Docenko87@users.noreply.github.com>
2023-09-10 15:21:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let question = prompt ("Как ваше имя?");
+alert ("Привет, его имя! " + question);
+
+// let bornDate = prompt ("Какой год вашего рождения?");
+// const year = ("2023");
+// alert ("Ваш возраст " + (year - bornDate + " лет"));
+
+// let question = prompt ("Укажите длину стороны квадрата в см");
+// answer = (question * 4); 
+// alert ("Периметр такого квадрата = " + answer + " см");
+
+// let question = prompt ("Укажите радиус окружности в см");
+// const PI = 3.14;
+// square = (question * question);
+// answer = (PI * square);
+// alert ("Площадь такой окружности = " + answer + " см");
+
+// let question = prompt ("Укажите расстояние между двумя городами в км");
+// let questionSecond = prompt ("За сколько часов вы хотите добраться?");
+// answer = question / questionSecond;
+// alert ("Ваша скорость должна быть не меньше " + answer + " км/ч чтобы успеть вовремя");
+
+// alert ("Конвертор валюты с долларов на рубли");
+// let exchange = prompt ("Укажите количество долларов");
+// const RUB = 100;
+// answer = exchange * RUB;
+// alert ("При текущем курсе доллара сумма = " + answer + " руб");
+
+// let volum = prompt ("Укажите объём флеш-карты в Гб");
+// volumMb = volum * 1024;
+// const MB = 820;
+// answer = volumMb / MB >> 0;
+// alert ("Количество файлов размером 820 Mb будет равно " + answer);
+
+// let money = prompt ("Сколько у вас денег в кошельке?");
+// let cost = prompt ("Сколько стоит шоколадка?");
+// answer = money / cost >> 0;
+// alert ("Вы можете купить вот столько шоколадок: " + answer);
+// answerEnd = ((money / cost) - (answer)) * 10 >> 0;
+// alert ("Остаток: " + answerEnd);
+
+// let number = prompt ("Введите трехзначное число");
+// alert ("Это число будет наооборот: " + number.split("").reverse().join(""));
+
+
+// let number = prompt ("Введите целое число");
+// number % 2 == 0 ? alert ("Число чётное") : alert("Число не чётное");
+
+// let n = prompt("Введите цело число");
+// alert (["Число чётное", "Число не чётное"][n * n % 2]);
+
